d6f84b349027722d93737d45fa8990ec73178793
Vasyl Kovalov <raid3r81@gmail.com>
2025-05-23 20:58:49 +0300

    Добавить асинхронное движение бомбы в класс Bomb
    В классе `Bomb` реализованы новые свойства `X`, `Y` и `Speed`, а также методы `MoveAsync`, `Destroy`, `Draw` и `Clear`. Метод `MoveAsync` отвечает за перемещение бомбы вниз с заданной скоростью, а метод `Destroy` деактивирует бомбу. Методы `Draw` и `Clear` используются для отображения и очистки бомбы на консоли.

---
+++ AsyncAwaitTaskPv421/Models/Bomb.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTaskPv421.Models;
+
+public class Bomb
+{
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+    public int Speed { get; set; } = 300;
+    private bool isActive = true;
+
+    public async Task MoveAsync()
+    {
+        Draw();
+        while (true) {
+            await Task.Delay(Speed);
+            if (!isActive)
+            {
+                break;
+            }
+            Clear();
+            Y++;
+            if (Y == Console.BufferHeight)
+            {
+                break;
+            }
+            Draw();
+        }
+    }
+
+    public void Destroy()
+    {
+        isActive = false;
+    }
+
+    public void Draw()
+    {
+        lock (Console.Out)
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("U");
+        }
+    }
+
+    public void Clear()
+    {
+        lock (Console.Out)
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(" ");
+        }
+    }
+
+}
